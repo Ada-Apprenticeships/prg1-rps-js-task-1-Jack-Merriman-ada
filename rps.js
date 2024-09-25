0af6d4bd79dffd90ade035326923c68e9c764fd6
Jack-Merriman-ada <jack.merriman@ada.ac.uk>
2024-09-25 10:37:16 +0000

    passed test_3_player2wins

--- rps.js
+++ rps.js
@@ -7,6 +7,12 @@ function rockPaperScissors(player1, player2) {
     return 'player1'
   } else if (player1 === 'paper' && player2 === 'rock') {
     return 'player1'
+  } else if (player2 === 'rock' && player1 === 'scissors') {
+    return 'player2'
+  } else if (player2 === 'scissors' && player1 === 'paper') {
+    return 'player2'
+  } else if (player2 === 'paper' && player1 === 'rock') {
+    return 'player2'
   }
 }
 
