f07a72ea06b09a89c0ee8f8e64310c291d3344a9
Jack-Merriman-ada <jack.merriman@ada.ac.uk>
2024-09-25 09:52:54 +0000

    passed test_1_draw, test_2_player1wins and test_4_RPSLS_Draw

--- rps.js
+++ rps.js
@@ -1,9 +1,16 @@
 function rockPaperScissors(player1, player2) {
-
-  
-   
+  if (player1 === player2) {
+    return 'draw'
+  } else if (player1 === 'rock' && player2 === 'scissors') {
+    return 'player1'
+  } else if (player1 === 'scissors' && player2 === 'paper') {
+    return 'player1'
+  } else if (player1 === 'paper' && player2 === 'rock') {
+    return 'player1'
+  }
 }
 
+//console.log(rockPaperScissors('paper', 'paper'))
 
 
 
@@ -16,4 +23,4 @@ function rockPaperScissors(player1, player2) {
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+} 
